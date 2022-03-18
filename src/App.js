712a394185c9bdbdc2acf003e133b72a8889ce45
Mark Momot <markmomot03@gmail.com>
2022-03-17 20:22:04 -0400

    Added about me info

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
       <header className="App-header">
         <h1>YOOOOOOOOOOOOO</h1>
         <h1>THIS IS MARK MOMOT'S WEBSITE</h1>
-        <button>About Me</button>
+        <h4>About Me</h4>
+        <p>My name is Mark Momot, I am 21 years old, and I am an aspiring software developer. This website demonstrates not only who I am, but my skills as a developer.</p>
       </header>
     </div>
   );
