b48cada7ca962da1dba53fd70bd7880c00be5e1b
Mark Momot <markmomot03@gmail.com>
2022-04-06 23:38:07 -0400

    Fixed some compiling errors

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 function Home(){
     return (
-        <div className="d-flex justify-content-center">
+        <div className="justify-content-center">
            <h1>YOOOOOOOOOOOOOOOOO</h1>
            <h1>THIS IS MARK MOMOT'S WEBSITE</h1>
         </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function NavBar(){
     return (
-        <nav>
+        <nav className="navbar">
             <Link to="/" className="btn btn-secondary">Home</Link>
             <Link to="/about" className="btn btn-secondary">About Me</Link>
         </nav>
