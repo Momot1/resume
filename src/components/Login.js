e3128bbcc96e077a44b6a0ac769710596ab2c5a0
Mark Momot <markmomot03@gmail.com>
2022-04-30 22:27:13 -0400

    Try to get API working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react"
+
 function Login(){
+
+    useEffect(() => {
+        fetch("https://markmomotbackend.herokuapp.com/birds")
+        .then(r => r.json())
+        .then(console.log)
+    }, [])
+
+
     return (
         <div>
             <h1>Login</h1>
