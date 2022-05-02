1d181f28969577bdf9b434684581ccb9c99f6dec
Momot1 <36507670+Momot1@users.noreply.github.com>
2022-05-02 15:12:52 -0400

    Added contact me button to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ function NavBar(){
             <Link to="/" className="btn btn-dark m-auto">Home</Link>
             <Link to="/about" className="btn btn-dark m-auto">About Me</Link>
             <Link to="/login" className="btn btn-dark m-auto">Login</Link>
+            <Link to="/contact" className="btn btn-dark m-auto">Contact Me</Link>
         </nav>
     )
 }
