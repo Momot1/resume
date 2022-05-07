547f76fbb511ee1025a54a915eae333a05cff8ac
Mark Momot <markmomot03@gmail.com>
2022-05-06 22:40:20 -0400

    Added signup functionality, now in testing

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,32 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
 
 function Signup(){
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        birthday: "",
+        name: "",
+        password: "",
+        password_confirmation: ""
+    })
+
     function onSignup(e){
         e.preventDefault()
+
+        fetch("http://markmomotbackend.herokuapp.com/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(resp => resp.json())
+        .then(console.log())
+    }
+
+    function updateForm(e, input){
+        setFormData({...formData, [input]: e.target.value})
     }
 
 
@@ -10,17 +34,17 @@ function Signup(){
         <div>
             <form onSubmit={onSignup}>
                 <label>Name: </label>
-                <input type="text" placeholder="Full Name"/><br/>
+                <input type="text" placeholder="Full Name" value={formData.name} onChange={e => updateForm(e, "name")}/><br/>
                 <label>Username: </label>
-                <input type="text" placeholder="Username"/><br/>
+                <input type="text" placeholder="Username" value={formData.username} onChange={e => updateForm(e, "username")}/><br/>
                 <label>Email: </label>
-                <input type="email" placeholder="Email"/><br/>
+                <input type="email" placeholder="Email" value={formData.email} onChange={e => updateForm(e, "email")}/><br/>
                 <label>Birthday: </label>
-                <input type="date" placeholder="Birthday"/><br/>
+                <input type="date" placeholder="Birthday" value={formData.birthday} onChange={e => updateForm(e, "birthday")}/><br/>
                 <label>Password: </label>
-                <input type="password" placeholder="Password"/><br/>
+                <input type="password" placeholder="Password" value={formData.password} onChange={e => updateForm(e, "password")}/><br/>
                 <label>Confirm Password: </label>
-                <input type="password" placeholder="Confirm Password"/><br/>
+                <input type="password" placeholder="Confirm Password" value={formData.password_confirmation} onChange={e => updateForm(e, "password_confirmation")}/><br/>
                 <button type="submit">Sign Up</button><br/>
                 <Link to="/login">Already have an account? Login</Link>
             </form>
