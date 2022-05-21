275f45f39f8532a1e896a62a6d6ef89fd99a5311
Mark Momot <markmomot03@gmail.com>
2022-05-20 20:24:00 -0400

    Fixed not compiling bug

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import { useState } from "react"
 
-function Signup(){
+function Signup({setUser}){
     const [formData, setFormData] = useState({
         username: "",
         email: "",
